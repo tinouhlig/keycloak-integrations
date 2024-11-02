b636180c114bb612a4d6d3f63cd4c0e2d797a684
Tino Uhlig <tino.uhlig@power.cloud>
2024-11-02 17:38:41 +0100

    clean up

--- java-spring/src/main/java/com/example/spring/access/interceptor/AccessInterceptor.java
+++ java-spring/src/main/java/com/example/spring/access/interceptor/AccessInterceptor.java
@@ -6,8 +6,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
